189a2a71d720438f310ebb323211fd76d3392fd6
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2023-08-15 15:03:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { foodObject } from "./foods";
+
+let foodImages = [];
+for (let food in foodObject) {
+  foodImages.push(
+    <img
+      alt={foodObject[food].name}
+      key={foodObject[food].name}
+      src={foodObject[food].img}
+      onClick={displayFoodInfo}
+    />
+  );
+}
+
+function displayFoodInfo(e) {
+  let foodName = e.target.alt;
+  document.getElementById("food-text").innerHTML =
+    "This is " +
+    foodName +
+    " and it's " +
+    foodObject[foodName].tastiness +
+    "! <br /> <br />" +
+    foodObject[foodName].furtherInfo;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Click on the foods for more info!</h1>
       </header>
+      <main>
+        <div id="food-imgs">{foodImages}</div>
+        <div>
+          <p id="food-text"></p>
+        </div>
+      </main>
+      <footer>
+        <p>
+          All images from{" "}
+          <a target="_blank" rel="noreferrer" href="https://unsplash.com/">
+            Unsplash
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/foods.js
@@ -0,0 +1,29 @@
+let cakeRecipe =
+  "Here are the ingredients: <br /> <ul><li>200g caster sugar</li><li>300g butter</li><li>4 eggs</li><li>200g self-raising flour</li><li>1tsp baking powder</li><li>2 tbsp milk</li><li>140g icing sugar</li><li>170g strawberry jam</li></ul><br />Click <a href='https://www.bbcgoodfood.com/recipes/classic-victoria-sandwich-recipe'>here</a> for the recipe!";
+let riceInfo =
+  "Did you know?<br /> Rice can be eaten as a savoury side for main dish, or you can add sugar and milk to make rice pudding!";
+let mangoTip =
+  "Top tip: <br />Check out <a target='_blank' href='https://www.bbcgoodfood.com/recipes/how-to-cut-a-mango'>this link</a> to learn how to cut a mango!";
+
+let foodObject = {
+  cake: {
+    name: "cake",
+    tastiness: "delicious",
+    img: "/images/cake.jpg",
+    furtherInfo: cakeRecipe,
+  },
+  rice: {
+    name: "rice",
+    tastiness: "a staple all over the world",
+    img: "/images/rice.jpg",
+    furtherInfo: riceInfo,
+  },
+  mango: {
+    name: "mango",
+    tastiness: "juicy",
+    img: "/images/mango.jpg",
+    furtherInfo: mangoTip,
+  },
+};
+
+export { foodObject };
